bcd897bdb9835197e77cdf5a0767102f2f83194b
Mohamed Sheikh <MohamedS@Mohameds-MBP.lan>
2018-07-29 23:57:52 +0100

    Minor tweak to format of groups appearance

--- src/championsLeagueTeamGenerator/GroupStages.java
+++ src/championsLeagueTeamGenerator/GroupStages.java
@@ -29,43 +29,43 @@ public class GroupStages {
 		groupA.put(2, secondTier.xx.get(1));
 		groupA.put(3, thirdTier.xx.get(1));
 		groupA.put(4, fourthTier.xx.get(1));
-		System.out.println(groupA);
+		System.out.println("Group A " + groupA);
 		
 		groupB.put(1, topTier.xx.get(2));
 		groupB.put(2, secondTier.xx.get(2));
 		groupB.put(3, thirdTier.xx.get(2));
 		groupB.put(4, fourthTier.xx.get(2));
-		System.out.println(groupB);
+		System.out.println("Group B " + groupB);
 		
 		groupC.put(1, topTier.xx.get(3));
 		groupC.put(2, secondTier.xx.get(3));
 		groupC.put(3, thirdTier.xx.get(3));
 		groupC.put(4, fourthTier.xx.get(3));
-		System.out.println(groupC);
+		System.out.println("Group C " + groupC);
 		
 		groupD.put(1, topTier.xx.get(4));
 		groupD.put(2, secondTier.xx.get(4));
 		groupD.put(3, thirdTier.xx.get(4));
 		groupD.put(4, fourthTier.xx.get(4));
-		System.out.println(groupD);
+		System.out.println("Group D " + groupD);
 		
 		groupE.put(1, topTier.xx.get(5));
 		groupE.put(2, secondTier.xx.get(5));
 		groupE.put(3, thirdTier.xx.get(5));
 		groupE.put(4, fourthTier.xx.get(5));
-		System.out.println(groupE);
+		System.out.println("Group E " + groupE);
 		
 		groupF.put(1, topTier.xx.get(6));
 		groupF.put(2, secondTier.xx.get(6));
 		groupF.put(3, thirdTier.xx.get(6));
 		groupF.put(4, fourthTier.xx.get(6));
-		System.out.println(groupF);
+		System.out.println("Group F " + groupF);
 	
 		groupG.put(1, topTier.xx.get(7));
 		groupG.put(2, secondTier.xx.get(7));
 		groupG.put(3, thirdTier.xx.get(7));
 		groupG.put(4, fourthTier.xx.get(7));
-		System.out.println(groupG);
+		System.out.println("Group G " + groupG);
 
 	}
 }
